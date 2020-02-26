a031816a534bf99ddfaa30915538e95aacb9b19d
Takuro Ashie <ashie@clear-code.com>
2020-02-26 09:45:28 +0900

    Make sure to expand STAGING_DIR & DOWNLOADS_DIR to absolute path
    Signed-off-by: Takuro Ashie <ashie@clear-code.com>

--- td-agent/config.rb
+++ td-agent/config.rb
@@ -1,7 +1,7 @@
 PACKAGE_NAME = "td-agent"
 PACKAGE_VERSION = "3.6.0"
 
-DOWNLOADS_DIR = "downloads"
-STAGING_DIR = ENV["TD_AGENT_STAGING_PATH"] || "staging"
+DOWNLOADS_DIR = File.expand_path(ENV["TD_AGENT_DOWNLOADS_PATH"] || "downloads")
+STAGING_DIR   = File.expand_path(ENV["TD_AGENT_STAGING_PATH"]   || "staging")
 
 FLUENTD_REVISION = "9d113029d4550ce576d8825bfa9612aa3e55bff0" # v1.9.2
