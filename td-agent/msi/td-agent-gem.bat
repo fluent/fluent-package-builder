3546ab98188c47634223e08392d1a8cccfc729b6
Takuro Ashie <ashie@clear-code.com>
2020-03-25 15:43:03 +0900

    MSI: Add batch files to reintroduce some missing commands
      * td-agent
      * td-agent-gem
    
    Signed-off-by: Takuro Ashie <ashie@clear-code.com>

---
+++ td-agent/msi/td-agent-gem.bat
@@ -0,0 +1,3 @@
+@echo off
+set TD_AGENT_BINDIR=%~dp0
+%TD_AGENT_BINDIR%fluent-gem %*
---
+++ td-agent/msi/td-agent.bat
@@ -0,0 +1,6 @@
+@echo off
+set TD_AGENT_TOPDIR=%~dp0\..
+set PATH="%~dp0";%PATH%
+set FLUENT_CONF=%TD_AGENT_TOPDIR%\etc\td-agent\td-agent.conf
+set FLUENT_PLUGIN=%TD_AGENT_TOPDIR%\etc\td-agent\plugin
+%TD_AGENT_TOPDIR%\bin\fluentd %*
