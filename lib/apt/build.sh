e0007c314ff80f173e59540a152b6d4904e19790
Kentaro Hayashi <kenhys@gmail.com>
2020-05-07 12:48:07 +0900

    deb: use more strict expression to rewrite distribution

--- lib/apt/build.sh
+++ lib/apt/build.sh
@@ -75,7 +75,7 @@ run mkdir -p ~/.lintian/profiles/td-agent/
 run cp "debian/lintian/td-agent/${distribution}.profile" ~/.lintian/profiles/td-agent/${distribution}.profile
 # export DEB_BUILD_OPTIONS=noopt
 DISTRIBUTION=`lsb_release -c | cut -f2`
-sed -i'' -e "s/unstable/$DISTRIBUTION/" debian/changelog
+sed -i'' -E "s/^($PACKAGE \(\S+\)) unstable;/\1 $DISTRIBUTION;/g" debian/changelog
 if [ "${DEBUG:-no}" = "yes" ]; then
   run debuild -us -uc --lintian-opts --profile td-agent/${distribution}
 else
