0fdd04aa935cb12e92179d7ce24a63c229452698
Kentaro Hayashi <hayashi@clear-code.com>
2020-08-04 13:54:59 +0900

    deb: test deb package by piuparts
    There are some pitfall to achieve
    
    * use --priviledged to allow debootstrap
    * deb is copied into /tmp because /fluentd/ is
    read-only file system.
    * keyring must be created by gnupg1 because
    gnupg2 default keyring format is changed to keybox.

--- td-agent/apt/install-test.sh
+++ td-agent/apt/install-test.sh
@@ -12,13 +12,44 @@ case ${code_name} in
   xenial|bionic|focal)
     distribution=ubuntu
     channel=universe
+    mirror=http://archive.ubuntu.com/ubuntu/
     ;;
   buster)
     distribution=debian
     channel=main
+    mirror=http://deb.debian.org/debian
     ;;
 esac
 apt install -V -y \
   ${repositories_dir}/${distribution}/pool/${code_name}/${channel}/*/*/*_${architecture}.deb
 
 td-agent --version
+
+find ${repositories_dir}
+case ${code_name} in
+    xenial)
+	apt install -V -y piuparts mount gnupg curl eatmydata
+	gpg_command=gpg
+	;;
+    *)
+	apt install -V -y piuparts mount gnupg1 curl eatmydata
+	gpg_command=gpg1
+	;;
+esac
+curl https://packages.treasuredata.com/GPG-KEY-td-agent > td-agent.gpg
+TD_AGENT_KEYRING=/usr/share/keyrings/td-agent-archive-keyring.gpg
+${gpg_command} --no-default-keyring --keyring $TD_AGENT_KEYRING --import td-agent.gpg
+CHROOT=/var/lib/chroot/${code_name}-root
+mkdir -p $CHROOT
+debootstrap ${code_name} $CHROOT ${mirror}
+cp $TD_AGENT_KEYRING $CHROOT/etc/apt/trusted.gpg.d/
+chmod 644 $CHROOT/etc/apt/trusted.gpg.d/td-agent-archive-keyring.gpg
+chroot $CHROOT apt install -V -y libyaml-0-2
+package=${repositories_dir}/${distribution}/pool/${code_name}/${channel}/*/*/*_${architecture}.deb
+cp ${package} /tmp
+piuparts --distribution=${code_name} \
+	 --existing-chroot=${CHROOT} \
+	 --keyring=$TD_AGENT_KEYRING \
+	 --mirror="http://packages.treasuredata.com/4/${distribution}/${code_name}/ ${code_name} contrib" \
+	 --skip-logrotatefiles-test \
+	 /tmp/*_${architecture}.deb
