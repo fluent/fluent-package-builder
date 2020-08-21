feed6f48aa682adf7692cfde5c8c9a8f9a4b7291
Kentaro Hayashi <hayashi@clear-code.com>
2020-08-21 16:04:18 +0900

    serverspec windows: add template spec

---
+++ serverspec/windows/td-agent.rb
@@ -0,0 +1,17 @@
+require_relative "../spec_helper"
+require "bundler"
+
+describe package("td-agent") do
+  it { should be_installed }
+end
+
+describe "gem files" do
+  lock_path = File.join(File.dirname(File.dirname(File.dirname(__FILE__))),
+                        "gemfiles/windows/Gemfile.lock")
+  parser = Bundler::LockfileParser.new(Bundler.read_file(lock_path))
+  parser.specs.each do |spec|
+    describe package("#{spec.name}") do
+      it { should be_installed.by('gem') }
+    end
+  end
+end
