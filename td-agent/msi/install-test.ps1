74fe6cc4e7110cb77aa0425a6ea419c7cf2c1b89
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2020-08-21 18:51:21 +0900

    windows: serversepc: Execute serverspec on GitHub workflows
    But none of specs is green... :cry:
    
    Signed-off-by: Hiroshi Hatake <cosmo0920.oucc@gmail.com>

--- td-agent/msi/install-test.ps1
+++ td-agent/msi/install-test.ps1
@@ -8,3 +8,7 @@ Start-Process msiexec -ArgumentList "/i", $msi, "/quiet" -Wait -NoNewWindow
 $ENV:PATH="C:\\opt\\td-agent\\bin;" + $ENV:PATH
 
 td-agent --version
+
+td-agent-gem install serverspec
+$ENV:INSTALLATION_TEST=$TRUE
+cd C:\fluentd; rake serverspec:windows
