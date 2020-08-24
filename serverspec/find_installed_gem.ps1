5e7484a5f67b553e0733267fcabce104d77d837b
Kentaro Hayashi <hayashi@clear-code.com>
2020-08-24 14:23:11 +0900

    serverspec windows: check gem name correctly
    It excludes duplicated prefix.

--- serverspec/find_installed_gem.ps1
+++ serverspec/find_installed_gem.ps1
@@ -3,7 +3,7 @@ function FindInstalledGem
   param($gemName, $gemVersion)
 
   $nameVer = $(Invoke-Expression "gem list --local" | Select-String "$gemName").Line
-  if ($nameVer.StartsWith($gemName)) {
+  if ($nameVer.StartsWith("$gemName ")) {
     if ($gemVersion) {
       if ($nameVer.EndsWith("$gemVersion)")) {
         $true
