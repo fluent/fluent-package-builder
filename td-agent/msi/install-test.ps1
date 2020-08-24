b672ad7be3a8b91fab5bcb0c97709f49ec9318b6
Kentaro Hayashi <hayashi@clear-code.com>
2020-08-24 13:24:06 +0900

    serverspec windows: copy .ps1 after installing serverspec

--- td-agent/msi/install-test.ps1
+++ td-agent/msi/install-test.ps1
@@ -9,9 +9,9 @@ $ENV:PATH="C:\\opt\\td-agent\\bin;" + $ENV:PATH
 
 td-agent --version
 
+td-agent-gem install serverspec
 $application = (Get-ChildItem -Path "c:\\opt" -Filter "find_installed_application.ps1" -Recurse -Name)
 $destination = (Get-Item (Join-Path "c:\\opt" $application)).DirectoryName
 Copy-Item "C:\\fluentd\\serverspec\\find_installed_gem.ps1" $destination
-td-agent-gem install serverspec
 $ENV:INSTALLATION_TEST=$TRUE
 cd C:\fluentd; rake serverspec:windows
