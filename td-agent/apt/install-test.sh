29cd08d617155bc31f05debf1b15a75b657893a6
Kentaro Hayashi <hayashi@clear-code.com>
2020-08-26 14:06:30 +0900

    deb: ensure to clean apt metadata

--- td-agent/apt/install-test.sh
+++ td-agent/apt/install-test.sh
@@ -98,6 +98,7 @@ cd /fluentd && rake serverspec:linux
 echo "td-agent-apt-source test"
 apt_source_repositories_dir=/fluentd/td-agent-apt-source/apt/repositories
 apt purge -y td-agent
+apt clean all
 apt_source_package=${apt_source_repositories_dir}/${distribution}/pool/${code_name}/${channel}/*/*/td-agent-apt-source*_all.deb
 apt install -V -y ${apt_source_package}
 apt update
