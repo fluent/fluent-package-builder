c63dd5e5b5cc8b542f1c07fce617093b86583761
Kentaro Hayashi <hayashi@clear-code.com>
2020-09-17 11:26:28 +0900

    serverspec: limit repeated times to wait response

--- td-agent/yum/serverspec-test.sh
+++ td-agent/yum/serverspec-test.sh
@@ -17,6 +17,7 @@ version=$(cat /etc/system-release-cpe | awk '{print substr($0, index($1, "o"))}'
 ENABLE_SERVERSPEC_TEST=1
 ENABLE_KAFKA_TEST=1
 JAVA_JRE=java-11-openjdk
+N_POLLING=30
 case ${distribution} in
   amazon)
     case ${version} in
@@ -75,19 +76,31 @@ EOF
         yum update && yum install -y confluent-community-2.12 ${JAVA_JRE} nc
 	export KAFKA_OPTS=-Dzookeeper.4lw.commands.whitelist=ruok
 	/usr/bin/zookeeper-server-start /etc/kafka/zookeeper.properties  &
-	while true ; do
+	n=1
+        while true ; do
 	    sleep 1
 	    status=$(echo ruok | nc localhost 2181)
 	    if [ "$status" = "imok" ]; then
 		break
 	    fi
+            n=$((n + 1))
+	    if [ $n -ge $N_POLLING ]; then
+		echo "failed to get response from zookeeper-server"
+		exit 1
+	    fi
 	done
 	/usr/bin/kafka-server-start /etc/kafka/server.properties &
+	n=1
 	while true ; do
 	    sleep 1
 	    status=$(/usr/bin/zookeeper-shell localhost:2181 ls /brokers/ids | sed -n 6p)
 	    if [ "$status" = "[0]" ]; then
 		break
+	    fi
+            n=$((n + 1))
+	    if [ $n -ge $N_POLLING ]; then
+		echo "failed to get response from kafka-server"
+		exit 1
 	    fi
 	done
 	/usr/bin/kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
