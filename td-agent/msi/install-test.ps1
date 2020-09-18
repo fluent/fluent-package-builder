6c004513827aac997f5d2eeaef26b9a59fb0f7ae
Kentaro Hayashi <hayashi@clear-code.com>
2020-09-18 17:38:00 +0900

    CI msi: split steps to test
    It also add existence check of .conf and .log

--- td-agent/msi/install-test.ps1
+++ td-agent/msi/install-test.ps1
@@ -9,14 +9,28 @@ $ENV:PATH="C:\\opt\\td-agent\\bin;" + $ENV:PATH
 
 td-agent --version
 
-td-agent-gem install --no-document serverspec
-$application = (Get-ChildItem -Path "c:\\opt" -Filter "find_installed_application.ps1" -Recurse -Name)
-$destination = (Get-Item (Join-Path "c:\\opt" $application)).DirectoryName
-Copy-Item "C:\\fluentd\\serverspec\\find_installed_gem.ps1" $destination
-$ENV:INSTALLATION_TEST=$TRUE
-cd C:\fluentd; rake serverspec:windows
-
+$msi -Match "td-agent-([0-9\.]+)-.+\.msi"
+$name = "Td-agent v" + $matches[1]
+Write-Host "Uninstalling ...${name}"
+Get-CimInstance -Class Win32_Product -Filter "Name='${name}'" | Invoke-CimMethod -MethodName Uninstall
 $exitcode = $LASTEXITCODE
 if ($exitcode -ne 0) {
     [Environment]::Exit($exitcode)
 }
+Write-Host "Succeeded to uninstall ${name}"
+
+# td-agent.conf should not be removed
+$conf = (Get-ChildItem -Path "c:\\opt" -Filter "td-agent.conf" -Recurse -Name)
+if ($conf -ne "td-agent\\etc\\td-agent\\td-agent.conf") {
+  Write-Host "Failed to find td-agent.conf"
+  [Environment]::Exit(1)
+}
+Write-Host "Succeeded to find td-agent.conf"
+
+# td-agent.log should not be removed
+$conf = (Get-ChildItem -Path "c:\\opt" -Filter "td-agent.log" -Recurse -Name)
+if ($conf -ne "td-agent\\td-agent.log") {
+  Write-Host "Failed to find td-agent.log"
+  [Environment]::Exit(1)
+}
+Write-Host "Succeeded to find td-agent.log"
