a18d20305645621d55f57e39184f2e9d40617673
Kentaro Hayashi <hayashi@clear-code.com>
2020-10-26 13:14:19 +0900

    msi: show td-agent and fluentd version
    Example:
    
      $ /usr/sbin/td-agent --version
      td-agent 4.0.1 fluentd 1.11.2 (2f80f6cf0a4aa44b9795cd6a66894a6b8fa70d06)

---
+++ td-agent/msi/assets/td-agent-version.rb
@@ -0,0 +1,6 @@
+td_agent_config = File.expand_path(File.join(File.dirname(__FILE__), "../share/config"))
+require td_agent_config
+Dir.glob("#{ENV['TD_AGENT_TOPDIR'].gsub(/\\/, '/')}/lib/ruby/**/bundler/**/fluent/version.rb").each do |v|
+  require v.delete_suffix(".rb")
+end
+puts "td-agent #{PACKAGE_VERSION} fluentd #{Fluent::VERSION} (#{FLUENTD_REVISION})"
--- td-agent/msi/assets/td-agent.bat
+++ td-agent/msi/assets/td-agent.bat
@@ -3,4 +3,11 @@ set TD_AGENT_TOPDIR=%~dp0\..
 set PATH="%~dp0";%PATH%
 set FLUENT_CONF=%TD_AGENT_TOPDIR%\etc\td-agent\td-agent.conf
 set FLUENT_PLUGIN=%TD_AGENT_TOPDIR%\etc\td-agent\plugin
+set TD_AGENT_VERSION=%TD_AGENT_TOPDIR%\bin\td-agent-version.rb
+for %%p in (%*) do (
+    if "%%p"=="--version" (
+        ruby "%TD_AGENT_VERSION%"
+        exit 0
+    )
+)
 "%TD_AGENT_TOPDIR%\bin\fluentd" %*
