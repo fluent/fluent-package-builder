86abba2850ad6f4bf7549b17e7cdda573689915e
Kentaro Hayashi <kenhys@gmail.com>
2021-02-16 11:17:40 +0900

    Support Ubuntu 21.04 (Hirsute)

--- td-agent/apt/commonvar.sh
+++ td-agent/apt/commonvar.sh
@@ -9,7 +9,7 @@ case ${code_name} in
     mirror=http://archive.ubuntu.com/ubuntu/
     java_jdk=openjdk-8-jre
     ;;
-  bionic|focal)
+  bionic|focal|hirsure)
     distribution=ubuntu
     channel=universe
     mirror=http://archive.ubuntu.com/ubuntu/
