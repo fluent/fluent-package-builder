cf2c941ffe77829acba409cfcbe6a84ecb2a59a1
Takuro Ashie <ashie@clear-code.com>
2021-05-19 11:35:13 +0900

    convert-artifacts-layout.sh: Move to "artifacts" directory
    Signed-off-by: Takuro Ashie <ashie@clear-code.com>

--- td-agent/convert-artifacts-layout.sh
+++ td-agent/convert-artifacts-layout.sh
@@ -10,6 +10,7 @@ set -ex
 
 TD_AGENT_DIR=$(dirname $(realpath $0))
 REPOSITORY_TYPE=""
+ARTIFACTS_DIR="artifacts"
 case $1 in
     apt|deb)
 	REPOSITORY_TYPE=apt
@@ -18,13 +19,13 @@ case $1 in
 	    case $d in
 		buster)
 		    # e.g. mapping debian/pool/buster/main/t/td-agent/ => 4/debian/buster/pool/contrib/t/td-agent
-		    mkdir -p $REPOSITORY_PATH/4/debian/$d/pool/contrib/t/td-agent
-		    find $REPOSITORY_PATH/debian/pool/$d -name '*.deb' -not -name '*dbgsym*' -exec cp {} $REPOSITORY_PATH/4/debian/$d/pool/contrib/t/td-agent \;
+		    mkdir -p $ARTIFACTS_DIR/4/debian/$d/pool/contrib/t/td-agent
+		    find $REPOSITORY_PATH/debian/pool/$d -name '*.deb' -not -name '*dbgsym*' -exec cp {} $ARTIFACTS_DIR/4/debian/$d/pool/contrib/t/td-agent \;
 		    ;;
 		xenial|bionic|focal)
 		    # e.g. mapping ubuntu/pool/.../main/t/td-agent/ => 4/ubuntu/.../pool/contrib/t/td-agent
-		    mkdir -p $REPOSITORY_PATH/4/ubuntu/$d/pool/contrib/t/td-agent
-		    find $REPOSITORY_PATH/ubuntu/pool/$d -name '*.deb' -exec cp {} $REPOSITORY_PATH/4/ubuntu/$d/pool/contrib/t/td-agent \;
+		    mkdir -p $ARTIFACTS_DIR/4/ubuntu/$d/pool/contrib/t/td-agent
+		    find $REPOSITORY_PATH/ubuntu/pool/$d -name '*.deb' -exec cp {} $ARTIFACTS_DIR/4/ubuntu/$d/pool/contrib/t/td-agent \;
 		    ;;
 		*)
 		    exit 1
@@ -55,8 +56,8 @@ case $1 in
 			continue
 		    fi
 		    # e.g. mapping amazon/2/x86_64/Packages/ => 4/amazon/2/x86_64
-		    mkdir -p $REPOSITORY_PATH/4/$dist_dest/$release/$arch
-		    find $REPOSITORY_PATH/$dist/$release/$arch -name '*.rpm' -not -name '*debug*' -exec cp {} $REPOSITORY_PATH/4/$dist_dest/$release/$arch \;
+		    mkdir -p $ARTIFACTS_DIR/4/$dist_dest/$release/$arch
+		    find $REPOSITORY_PATH/$dist/$release/$arch -name '*.rpm' -not -name '*debug*' -exec cp {} $ARTIFACTS_DIR/4/$dist_dest/$release/$arch \;
 		done
 	    done
 	done
