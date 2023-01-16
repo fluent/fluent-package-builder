b7ddd36c664cccbf0895f2445e556661609079ae
Takuro Ashie <ashie@clear-code.com>
2023-01-16 15:49:12 +0900

    Bump up the package version to 5.0
    The mater branch is now going to develop v5.0.
    
    Signed-off-by: Takuro Ashie <ashie@clear-code.com>

--- td-agent/config.rb
+++ td-agent/config.rb
@@ -1,5 +1,5 @@
 PACKAGE_NAME = "td-agent"
-PACKAGE_VERSION = "4.4.2"
+PACKAGE_VERSION = "5.0.0"
 
 FLUENTD_REVISION = 'e89092ce1132a933c12bb23fe8c9323c07ca81f5' # v1.15.3
 FLUENTD_LOCAL_GEM_REPO = "file://" + File.expand_path(File.join(__dir__, "local_gem_repo"))
