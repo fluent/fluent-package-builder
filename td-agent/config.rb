6655bc2b94e6acff835c8da5f57db8280fb85a20
Takuro Ashie <ashie@clear-code.com>
2023-04-03 11:26:02 +0900

    Update Ruby to v3.2.2 (#438)
    Signed-off-by: Takuro Ashie <ashie@clear-code.com>

--- td-agent/config.rb
+++ td-agent/config.rb
@@ -17,8 +17,8 @@ OPENSSL_FOR_MACOS_SHA256SUM = "6c13d2bf38fdf31eac3ce2a347073673f5d63263398f1f69d
 BUNDLER_VERSION= "2.3.26"
 
 # https://www.ruby-lang.org/en/downloads/ (tar.gz)
-BUNDLED_RUBY_VERSION = "3.2.0"
-BUNDLED_RUBY_SOURCE_SHA256SUM = "daaa78e1360b2783f98deeceb677ad900f3a36c0ffa6e2b6b19090be77abc272"
+BUNDLED_RUBY_VERSION = "3.2.2"
+BUNDLED_RUBY_SOURCE_SHA256SUM = "96c57558871a6748de5bc9f274e93f4b5aad06cd8f37befa0e8d94e7b8a423bc"
 
 BUNDLED_RUBY_PATCHES = [
   # An example entry:
@@ -26,8 +26,8 @@ BUNDLED_RUBY_PATCHES = [
 ]
 
 # https://rubyinstaller.org/downloads/ (7-ZIP ARCHIVES)
-BUNDLED_RUBY_INSTALLER_X64_VERSION = "3.2.0-1"
-BUNDLED_RUBY_INSTALLER_X64_SHA256SUM = "c89a52859e9b008f73ad2bb2bce57b70b40ed90ccce68eefe16e49803bbb2c41"
+BUNDLED_RUBY_INSTALLER_X64_VERSION = "3.2.2-1"
+BUNDLED_RUBY_INSTALLER_X64_SHA256SUM = "1f0f55ba0790676be6d701515a244159e9ca2d6314f1101560e3e8a277a2661e"
 
 # Patch files are assumed to be for Ruby's source tree, then applied to
 # lib/ruby/x.y.0 in RubyInstaller. So that "-p2" options will be passed
