efca7eafc39688b543783409ff4940811098e848
Kentaro Hayashi <hayashi@clear-code.com>
2023-05-11 13:36:54 +0900

    apt: remove deprecated piuparts stuff (#457)
    * xenial (16.04LTS) was already reached EOL.
    * do not skip piuparts for jammy (22.04)
    * use recent gnupg (old version is used for xenial, no need to do so)
    * remove unused bionic configuration
    * fix key verification failure of keyring path
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/apt/piuparts-test.sh
+++ fluent-package/apt/piuparts-test.sh
@@ -8,41 +8,23 @@ apt install -V -y lsb-release
 . $(dirname $0)/commonvar.sh
 
 find ${repositories_dir}
-case ${code_name} in
-    xenial)
-	apt install -V -y piuparts mount gnupg curl eatmydata apt-transport-https
-	gpg_command=gpg
-	;;
-    jammy)
-	# TODO: Remove when repository for jammy has been deployed
-	echo "skip piuparts test for jammy"
-	exit 0
-	;;
-    *)
-	DEBIAN_FRONTEND=noninteractive apt install -V -y piuparts mount gnupg1 curl eatmydata
-	gpg_command=gpg1
-	;;
-esac
+DEBIAN_FRONTEND=noninteractive apt install -V -y piuparts mount gnupg curl eatmydata
+gpg_command=gpg
 curl https://packages.treasuredata.com/GPG-KEY-td-agent > td-agent.gpg
 TD_AGENT_KEYRING=/usr/share/keyrings/td-agent-archive-keyring.gpg
 ${gpg_command} --no-default-keyring --keyring $TD_AGENT_KEYRING --import td-agent.gpg
 CHROOT=/var/lib/chroot/${code_name}-root
 mkdir -p $CHROOT
-debootstrap ${code_name} $CHROOT ${mirror}
-cp $TD_AGENT_KEYRING $CHROOT/etc/apt/trusted.gpg.d/
-chmod 644 $CHROOT/etc/apt/trusted.gpg.d/td-agent-archive-keyring.gpg
+debootstrap --include=ca-certificates ${code_name} $CHROOT ${mirror}
+cp $TD_AGENT_KEYRING $CHROOT/usr/share/keyrings/
+chmod 644 $CHROOT/usr/share/keyrings/td-agent-archive-keyring.gpg
 chroot $CHROOT apt install -V -y libyaml-0-2
-if [ "${code_name}" = "bionic" ]; then
-   echo "deb http://archive.ubuntu.com/ubuntu bionic-updates main" > $CHROOT/etc/apt/sources.list
-   chroot $CHROOT apt update
-   chroot $CHROOT apt install -V -y libssl1.1
-fi
 package=${repositories_dir}/${distribution}/pool/${code_name}/${channel}/*/*/*_${architecture}.deb
 cp ${package} /tmp
+echo "deb [signed-by=/usr/share/keyrings/td-agent-archive-keyring.gpg] https://packages.treasuredata.com/4/${distribution}/${code_name}/ ${code_name} contrib" | tee $CHROOT/etc/apt/sources.list.d/td.list
 rm -rf $CHROOT/opt
 piuparts --distribution=${code_name} \
 	 --existing-chroot=${CHROOT} \
-	 --keyring=$TD_AGENT_KEYRING \
-	 --mirror="http://packages.treasuredata.com/4/${distribution}/${code_name}/ ${code_name} contrib" \
+	 --mirror="https://packages.treasuredata.com/4/${distribution}/${code_name}/ ${code_name} contrib" \
 	 --skip-logrotatefiles-test \
 	 /tmp/*_${architecture}.deb
