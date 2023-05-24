fbf9a68126b0cffd77e9be2d669f76c189a04686
Daijiro Fukuda <fukuda@clear-code.com>
2023-05-24 17:26:44 +0900

    dmg: fix service name
    Signed-off-by: Daijiro Fukuda <fukuda@clear-code.com>

--- fluent-package/dmg/install-test.sh
+++ fluent-package/dmg/install-test.sh
@@ -12,7 +12,7 @@ sudo launchctl load /Library/LaunchDaemons/fluentd.plist
 hdiutil detach /Volumes/fluent-package
 
 sleep 10 # Wait for Fluentd to start up and run correctly.
-test $(sudo launchctl list fluent-package | grep LastExitStatus | grep -oE "[0-9]+") = 0
+test $(sudo launchctl list fluentd | grep LastExitStatus | grep -oE "[0-9]+") = 0
 
 sudo launchctl unload /Library/LaunchDaemons/fluentd.plist
 sudo rm -rf /opt/fluent/ /etc/td-agent/ /var/log/td-agent /Library/LaunchDaemons/fluentd.plist
