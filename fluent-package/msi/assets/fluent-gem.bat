a71a2e66434c51e04595e94f56f09e327c832a6d
Kentaro Hayashi <hayashi@clear-code.com>
2023-05-25 13:33:25 +0900

    msi: migrate td-agent*.bat
    Before:
    
    c:\opt\td-agent\bin\td-agent.bat
    c:\opt\td-agent\bin\td-agent-gem.bat
    
    After:
    
    c:\opt\td-agent\fluentd.bat
    c:\opt\td-agent\fluent-gem.bat
    
    If td-agent-gem.bat was renamed to fluent-gem.bat, it conflict with
    c:\opt\td-agent\fluent-gem.bat which fluentd gem provides.
    Not to cause such a situation, put them under c:\opt\td-agent.
    
    To keep compatibility, td-agent.bat and td-agent-gem are provided
    as hardlink.
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

---
+++ fluent-package/msi/assets/fluent-gem.bat
@@ -0,0 +1,8 @@
+@echo off
+set FLUENT_PACKAGE_TOPDIR=%~dp0
+set FLUENT_PACKAGE_BINDIR=%FLUENT_PACKAGE_TOPDIR%bin
+set PATH="%FLUENT_PACKAGE_BINDIR%";%PATH%
+for /f "usebackq" %%i in (`%FLUENT_PACKAGE_BINDIR%\ruby.exe -rrbconfig -e "print RbConfig::CONFIG['ruby_version']"`) do set RUBY_VERSION=%%i
+set GEM_HOME=%FLUENT_PACKAGE_TOPDIR%lib\ruby\gems\%RUBY_VERSION%
+set GEM_PATH=%FLUENT_PACKAGE_TOPDIR%lib\ruby\gems\%RUBY_VERSION%
+%FLUENT_PACKAGE_BINDIR%\fluent-gem %*
--- fluent-package/msi/assets/fluentd.bat
+++ fluent-package/msi/assets/fluentd.bat
@@ -1,5 +1,5 @@
 @echo off
-set TD_AGENT_TOPDIR=%~dp0\..
+set TD_AGENT_TOPDIR=%~dp0
 set PATH="%~dp0";%PATH%
 set FLUENT_CONF=%TD_AGENT_TOPDIR%\etc\td-agent\td-agent.conf
 set FLUENT_PLUGIN=%TD_AGENT_TOPDIR%\etc\td-agent\plugin
