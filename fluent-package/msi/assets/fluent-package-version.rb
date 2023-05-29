d67947444e6ec3027539fc9dc843038db9f14be6
Kentaro Hayashi <hayashi@clear-code.com>
2023-05-29 14:20:47 +0900

    msi: fix package name
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/msi/assets/fluent-package-version.rb
+++ fluent-package/msi/assets/fluent-package-version.rb
@@ -4,4 +4,4 @@ require 'fluent/version'
 td_agent_config = File.expand_path(File.join(File.dirname(__FILE__), "../share/config"))
 require td_agent_config
 
-puts "td-agent #{PACKAGE_VERSION} fluentd #{Fluent::VERSION} (#{FLUENTD_REVISION})"
+puts "fluent-package #{PACKAGE_VERSION} fluentd #{Fluent::VERSION} (#{FLUENTD_REVISION})"
