b1f113816f0a0eb1133ae8068976d70f85ad4afc
Daijiro Fukuda <fukuda@clear-code.com>
2023-06-13 14:57:42 +0900

    deb: reinforce test with systemd (#497)
    Add tests for migration from v4 and update to the future version.
    
    Signed-off-by: Daijiro Fukuda <fukuda@clear-code.com>

---
+++ fluent-package/apt/systemd-test/install-newly.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -exu
+
+. $(dirname $0)/../commonvar.sh
+
+sudo apt install -V -y \
+    /vagrant/${distribution}/pool/${code_name}/${channel}/*/*/fluent-package_*_${architecture}.deb
+
+systemctl status fluentd
+
+sudo apt remove -y fluent-package
+
+! systemctl status fluentd
