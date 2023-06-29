e6ff86fad711e2b861ad6e419f7455355235b838
Kentaro Hayashi <hayashi@clear-code.com>
2023-06-29 17:37:18 +0900

    msi: migrate to FLUENT_PACKAGE_TOPDIR
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/msi/assets/fluentd.bat
+++ fluent-package/msi/assets/fluentd.bat
@@ -1,22 +1,19 @@
 @echo off
 if "%~nx0" == "td-agent.bat" (
   set FLUENT_PACKAGE_TOPDIR=%~dp0..\
-  set TD_AGENT_TOPDIR=%~dp0..\..\td-agent
 ) else (
   set FLUENT_PACKAGE_TOPDIR=%~dp0
-  set TD_AGENT_TOPDIR=%~dp0..\td-agent
 )
 
 @rem Convert path separator from backslash to forwardslash
 setlocal enabledelayedexpansion
 set FLUENT_PACKAGE_TOPDIR="FLUENT_PACKAGE_TOPDIR=!FLUENT_PACKAGE_TOPDIR:\=/!"
-set TD_AGENT_TOPDIR="TD_AGENT_TOPDIR=!TD_AGENT_TOPDIR:\=/!"
 endlocal
 
 set PATH=%FLUENT_PACKAGE_TOPDIR%bin;%PATH%
 set PATH=%FLUENT_PACKAGE_TOPDIR%;%PATH%
-set FLUENT_CONF=%TD_AGENT_TOPDIR%/etc/td-agent/td-agent.conf
-set FLUENT_PLUGIN=%TD_AGENT_TOPDIR%/etc/td-agent/plugin
+set FLUENT_CONF=%FLUENT_PACKAGE_TOPDIR%/etc/fluent/fluentd.conf
+set FLUENT_PLUGIN=%FLUENT_PACKAGE_TOPDIR%/etc/fluent/plugin
 set FLUENT_PACKAGE_VERSION=%FLUENT_PACKAGE_TOPDIR%/bin/fluent-package-version.rb
 for %%p in (%*) do (
     if "%%p"=="--version" (
