319afd3b50d56a722cbd7683c2429e730fda284e
Kentaro Hayashi <hayashi@clear-code.com>
2023-07-11 18:27:06 +0900

    test: check cleanup status of .service
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/apt/systemd-test/install-newly.sh
+++ fluent-package/apt/systemd-test/install-newly.sh
@@ -11,4 +11,13 @@ systemctl status --no-pager fluentd
 
 sudo apt remove -y fluent-package
 
+if [ ! -e /etc/systemd/system/td-agent.service ]; then
+    echo "td-agent.service must exist"
+    exit 1
+fi
+if [ ! -e /etc/systemd/system/fluentd.service ]; then
+    echo "fluentd.service must exist"
+    exit 1
+fi
+
 ! systemctl status fluentd
