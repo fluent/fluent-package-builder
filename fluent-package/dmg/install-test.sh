670ce2d5b592be5d5507df43f48f93af09df4ada
Daijiro Fukuda <fukuda@clear-code.com>
2023-07-19 09:34:40 +0900

    Use out_secondary_file for secondary output (#486)
    To solve the warning message:
    
        Use different plugin for secondary ...
    
    ---------
    
    Signed-off-by: Daijiro Fukuda <fukuda@clear-code.com>

--- fluent-package/dmg/install-test.sh
+++ fluent-package/dmg/install-test.sh
@@ -13,6 +13,7 @@ hdiutil detach /Volumes/fluent-package
 
 sleep 10 # Wait for Fluentd to start up and run correctly.
 test $(sudo launchctl list fluentd | grep LastExitStatus | grep -oE "[0-9]+") = 0
+! grep -q -e '\[warn\]' -e '\[error\]' -e '\[fatal\]' /var/log/fluent/fluentd.log
 
 sudo launchctl unload /Library/LaunchDaemons/fluentd.plist
 sudo rm -rf /opt/fluent/ /etc/td-agent/ /var/log/td-agent /Library/LaunchDaemons/fluentd.plist
