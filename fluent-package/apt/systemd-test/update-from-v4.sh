18346c6f85adbd2c5fe375d86140294eb0095a10
Daijiro Fukuda <fukuda@clear-code.com>
2023-07-24 13:01:12 +0900

    deb: rpm: add log test for migration
    Signed-off-by: Daijiro Fukuda <fukuda@clear-code.com>

--- fluent-package/apt/systemd-test/update-from-v4.sh
+++ fluent-package/apt/systemd-test/update-from-v4.sh
@@ -57,6 +57,11 @@ test $(eval $env_vars && echo $FLUENT_PACKAGE_LOG_FILE) = "/var/log/fluent/td-ag
 test $(eval $env_vars && echo $FLUENT_PLUGIN) = "/etc/fluent/plugin"
 test $(eval $env_vars && echo $FLUENT_SOCKET) = "/var/run/fluent/fluentd.sock"
 
+# Test: No error logs
+# (v4 default config outputs 'warn' log, so we should check only 'error' and 'fatal' logs)
+sleep 3
+(! grep -q -e '\[error\]' -e '\[fatal\]' /var/log/td-agent/td-agent.log)
+
 # Uninstall
 sudo apt remove -y fluent-package
 (! systemctl status --no-pager td-agent)
