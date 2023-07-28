ebdcaf248f592d00a4f1046ec1027f0de934f656
Kentaro Hayashi <hayashi@clear-code.com>
2023-07-28 11:30:44 +0900

    deb rpm: verify to install the variants of apt-source
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/yum/systemd-test/install-newly.sh
+++ fluent-package/yum/systemd-test/install-newly.sh
@@ -4,8 +4,42 @@ set -exu
 
 . $(dirname $0)/commonvar.sh
 
-sudo $DNF install -y \
-    /vagrant/${distribution}/${DISTRIBUTION_VERSION}/x86_64/Packages/fluent-package-[0-9]*.rpm
+case $1 in
+  local)
+    sudo $DNF install -y \
+      /vagrant/${distribution}/${DISTRIBUTION_VERSION}/x86_64/Packages/fluent-package-[0-9]*.rpm
+    ;;
+  v5)
+    rpm --import https://packages.treasuredata.com/GPG-KEY-td-agent
+    rpm --import https://packages.treasuredata.com/GPG-KEY-fluent-package
+    sudo sh <<SCRIPT
+    cat > /etc/yum.repos.d/fluent-package.repo <<'EOF';
+[fluent-package]
+name=Fluentd Project
+baseurl=https://packages.treasuredata.com/lts/5/${distribution}/${DISTRIBUTION_VERSION}/\$basearch
+gpgcheck=1
+gpgkey=https://packages.treasuredata.com/GPG-KEY-td-agent
+       https://packages.treasuredata.com/GPG-KEY-fluent-package
+EOF
+SCRIPT
+    sudo $DNF install -y fluent-package
+    ;;
+  lts)
+    rpm --import https://packages.treasuredata.com/GPG-KEY-td-agent
+    rpm --import https://packages.treasuredata.com/GPG-KEY-fluent-package
+    sudo sh <<SCRIPT
+    cat > /etc/yum.repos.d/fluent-package-lts.repo <<'EOF';
+[fluent-package]
+name=Fluentd Project
+baseurl=https://packages.treasuredata.com/lts/5/${distribution}/${DISTRIBUTION_VERSION}/\$basearch
+gpgcheck=1
+gpgkey=https://packages.treasuredata.com/GPG-KEY-td-agent
+       https://packages.treasuredata.com/GPG-KEY-fluent-package
+EOF
+SCRIPT
+    sudo $DNF install -y fluent-package
+    ;;
+esac
 
 (! systemctl status --no-pager fluentd)
 sudo systemctl enable --now fluentd
