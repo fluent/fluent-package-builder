2b9099310738d84f95e0b5d222979d29a0a789e2
Kentaro Hayashi <hayashi@clear-code.com>
2023-09-13 10:53:21 +0900

    msi: support path which contains space or parenthesis
    When installing fluent-package to c:\Program Files, the space
    character must be escaped correctly.
    
    * sources.wxs
      * For xml, double quote (") must be &quot;
      * to escape double quote in literal, you must escape with ^&quote;
        it is treated as ^", so it is properly quoted to execute:
    
      "cmd" /c "fsutil ... "c:\Program Files .." "c:\Program Files...""
    
    * fluentd.bat,fluent-gem.bat
      * set FOO="..." assign extra double quote, it's troublesome because
      extra double quote will be included when concatenating path.
      The only way to avoid this problem is set "FOO=..." to assign it.
      you can assign to FOO without extra double quote.
      * to get RUBY_VERSION, use "usebackq" and execute command with
        escaped command literal explicitly.
         passed `^""C:\..." ....^"` literal are evaluated ^""C:\..."^".
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/msi/assets/fluentd.bat
+++ fluent-package/msi/assets/fluentd.bat
@@ -1,8 +1,8 @@
 @echo off
 if "%~nx0" == "td-agent.bat" (
-  set FLUENT_PACKAGE_TOPDIR=%~dp0..\
+  set "FLUENT_PACKAGE_TOPDIR=%~dp0..\"
 ) else (
-  set FLUENT_PACKAGE_TOPDIR=%~dp0
+  set "FLUENT_PACKAGE_TOPDIR=%~dp0"
 )
 
 @rem Convert path separator from backslash to forwardslash
@@ -11,9 +11,9 @@ set FLUENT_PACKAGE_TOPDIR=!FLUENT_PACKAGE_TOPDIR:\=/!
 
 set PATH=%FLUENT_PACKAGE_TOPDIR%bin;%PATH%
 set PATH=%FLUENT_PACKAGE_TOPDIR%;%PATH%
-set FLUENT_CONF=%FLUENT_PACKAGE_TOPDIR%/etc/fluent/fluentd.conf
-set FLUENT_PLUGIN=%FLUENT_PACKAGE_TOPDIR%/etc/fluent/plugin
-set FLUENT_PACKAGE_VERSION=%FLUENT_PACKAGE_TOPDIR%/bin/fluent-package-version.rb
+set "FLUENT_CONF=%FLUENT_PACKAGE_TOPDIR%/etc/fluent/fluentd.conf"
+set "FLUENT_PLUGIN=%FLUENT_PACKAGE_TOPDIR%/etc/fluent/plugin"
+set "FLUENT_PACKAGE_VERSION=%FLUENT_PACKAGE_TOPDIR%/bin/fluent-package-version.rb"
 for %%p in (%*) do (
     if "%%p"=="--version" (
         ruby "%FLUENT_PACKAGE_VERSION%"
