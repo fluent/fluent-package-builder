564a0fb851b677cef08eaa542035b617bd0709b3
Kentaro Hayashi <hayashi@clear-code.com>
2023-12-07 16:50:28 +0900

    github: use latest td-agent
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/yum/systemd-test/commonvar.sh
+++ fluent-package/yum/systemd-test/commonvar.sh
@@ -2,7 +2,7 @@
 
 distribution=$(cat /etc/system-release-cpe | awk '{print substr($0, index($1, "o"))}' | cut -d: -f2)
 version=$(cat /etc/system-release-cpe | awk '{print substr($0, index($1, "o"))}' | cut -d: -f4)
-td_agent_version=4.5.1
+td_agent_version=4.5.2
 
 case $distribution in
   amazon)
