125cc767062171c0eddf3b9a6ae34057b8ed7dd7
Kentaro Hayashi <hayashi@clear-code.com>
2024-02-19 18:20:33 +0900

    msi: guard multiple Fluentd instance
    Before:
    
      Even though fluentdwinsvc is running, you can execute duplicated
      Fluentd instance.
      It may cause inconsistency of buffer or pos file.
    
    After:
    
      Guard multiple Fluentd instance when fluendwinsvc service is
      running by default.
    
      Note that --force-running-multiple-instance option is specified, you
      can run additional Fluentd instance explicitly. (it is same as
      previous behavior)
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/msi/assets/fluentd.bat
+++ fluent-package/msi/assets/fluentd.bat
@@ -14,13 +14,33 @@ set PATH=%FLUENT_PACKAGE_TOPDIR%;%PATH%
 set "FLUENT_CONF=%FLUENT_PACKAGE_TOPDIR%/etc/fluent/fluentd.conf"
 set "FLUENT_PLUGIN=%FLUENT_PACKAGE_TOPDIR%/etc/fluent/plugin"
 set "FLUENT_PACKAGE_VERSION=%FLUENT_PACKAGE_TOPDIR%/bin/fluent-package-version.rb"
+set FLUENT_FORCE_RUN=0
+set FLUENT_ARGS=
 for %%p in (%*) do (
     if "%%p"=="--version" (
         ruby "%FLUENT_PACKAGE_VERSION%"
         goto last
     )
+    if "%%p"=="--force-running-multiple-instance" (
+        set /a FLUENT_FORCE_RUN=1
+    ) else (
+        set "FLUENT_ARGS=!FLUENT_ARGS! %%p"
+    )
 )
-"%FLUENT_PACKAGE_TOPDIR%/bin/fluentd" %*
+
+@rem Abort if the fluentdwinsvc service is running without --force option.
+sc query fluentdwinsvc | findstr RUNNING > nul
+if !ERRORLEVEL! equ 0 (
+    if %FLUENT_FORCE_RUN% equ 1 (
+        goto noguard
+    ) else (
+        echo Error: can't start duplicate Fluentd instance. Use --force-running-multiple-instance option explicitly.
+        exit /b 2
+    )
+)
+
+:noguard
+"%FLUENT_PACKAGE_TOPDIR%/bin/fluentd" %FLUENT_ARGS%
 endlocal
 
 :last
