f41c9e3c99cedfbd803c6815b7ff73711b8c0e84
Kentaro Hayashi <hayashi@clear-code.com>
2024-03-27 13:47:15 +0900

    github: extract test file as element of matrix (#636)
    Before:
    
      test.sh execute multiple test files.
      This behavior make it hard to distinct failure test cases.
    
    After:
    
      Use test.sh as launch script. It doesn't execute multiple test files.
      This change was introduced to make it easy to distinct each test
      cases on error.
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/apt/systemd-test/test.sh
+++ fluent-package/apt/systemd-test/test.sh
@@ -2,50 +2,34 @@
 
 if [ -z $1 ]; then
     echo "Error: Need to specify lxc image name."
-    echo "Ex.) $ ./test.sh ubuntu:20.04"
+    echo "Ex.) $ ./test.sh ubuntu:20.04 "
+    echo "Ex. CI) $ ./test.sh ubuntu:20.04 install-newly.sh local"
     exit 1
 fi
 
 image=$1
+test_file=$2
+apt_repo_type=$3
 dir="/host/fluent-package/apt/systemd-test"
 
 set -eux
 
-test_filenames=(
-    update-to-next-version.sh
-)
-
-if [ ! $image = "images:debian/12" ]; then
-    # As no bookworm package for v4, so execute upgrade test for other code name.
-    test_filenames+=(
-        update-from-v4.sh
-        update-to-next-version-with-backward-compat-for-v4.sh
-        downgrade-to-v4.sh
-    )
-fi
-
-for apt_repo_type in local v5 lts; do
-    echo -e "\nRun test: $apt_repo_type\n"
-    lxc launch $image target
-    sleep 5
-    lxc config device add target host disk source=$PWD path=/host
-    lxc list
-    lxc exec target -- $dir/setup.sh
-    lxc exec target -- $dir/install-newly.sh $apt_repo_type
-    lxc stop target
-    lxc delete target
-done
-
-for test_filename in ${test_filenames[@]}; do
-    echo -e "\nRun test: $test_filename\n"
-    lxc launch $image target
-    sleep 5
-    lxc config device add target host disk source=$PWD path=/host
-    lxc list
-    lxc exec target -- $dir/setup.sh
-    lxc exec target -- $dir/$test_filename
-    lxc stop target
-    lxc delete target
-done
-
+echo "::group::Run test: launch $image"
+lxc launch $image target
+sleep 5
+echo "::endgroup::"
+echo "::group::Run test: configure $image"
+lxc config device add target host disk source=$PWD path=/host
+lxc list
+echo "::endgroup::"
+echo "::group::Run test: setup $image"
+lxc exec target -- $dir/setup.sh
+echo "::endgroup::"
+echo "::group::Run test: $test_file $apt_repo_type on $image"
+lxc exec target -- $dir/$test_file $apt_repo_type
+echo "::endgroup::"
+echo "::group::Run test: cleanup $image"
+lxc stop target
+lxc delete target
+echo "::endgroup::"
 echo -e "\nAll Success!\n"
--- fluent-package/yum/systemd-test/test.sh
+++ fluent-package/yum/systemd-test/test.sh
@@ -3,47 +3,29 @@
 if [ -z $1 ]; then
     echo "Error: Need to specify distribution name."
     echo "Ex.) $ ./test.sh centos-7"
+    echo "Ex.CI) $ ./test.sh centos-7 install-newly.sh local"
     exit 1
 fi
 
 image=$1
+test_file=$2
+yum_repo_type=$3
 dir="/host/fluent-package/yum/systemd-test"
-
 set -eux
 
-test_filenames=(
-    update-to-next-version.sh
-)
-
-if [ ! $image = "images:amazonlinux/2023" ]; then
-    # As no AmazonLinux/2023 package for v4, so execute upgrade test for other images.
-    test_filenames+=(
-        update-from-v4.sh
-        update-to-next-version-with-backward-compat-for-v4.sh
-        downgrade-to-v4.sh
-    )
-fi
-
-for yum_repo_type in local v5 lts; do
-    echo -e "\nRun test: $yum_repo_type\n"
-    lxc launch $image target
-    sleep 5
-    lxc config device add target host disk source=$PWD path=/host
-    lxc list
-    lxc exec target -- $dir/install-newly.sh $yum_repo_type
-    lxc stop target
-    lxc delete target
-done
-
-for test_filename in ${test_filenames[@]}; do
-    echo -e "\nRun test: $test_filename\n"
-    lxc launch $image target
-    sleep 5
-    lxc config device add target host disk source=$PWD path=/host
-    lxc list
-    lxc exec target -- $dir/$test_filename
-    lxc stop target
-    lxc delete target
-done
-
+echo "::group::Run test: launch $image"
+lxc launch $image target
+sleep 5
+echo "::endgroup::"
+echo "::group::Run test: configure $image"
+lxc config device add target host disk source=$PWD path=/host
+lxc list
+echo "::endgroup::"
+echo "::group::Run test: $test_file $yum_repo_type on $image"
+lxc exec target -- $dir/$test_file $yum_repo_type
+echo "::endgroup::"
+echo "::group::Run test: cleanup $image"
+lxc stop target
+lxc delete target
+echo "::endgroup::"
 echo -e "\nAll Success!\n"
