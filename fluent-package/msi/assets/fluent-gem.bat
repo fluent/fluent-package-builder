efb8897f3b287ab028c029f640ae196b2b523a04
Daijiro Fukuda <fukuda@clear-code.com>
2024-06-27 10:38:55 +0900

    msi: refactor execution bat files
    * Add entire local scope to prevent changing global env vars
    * Remove unnecessary enabledelayedexpansion
    * Remove unnecessary `/`
    
    Signed-off-by: Daijiro Fukuda <fukuda@clear-code.com>

--- fluent-package/msi/assets/fluent-gem.bat
+++ fluent-package/msi/assets/fluent-gem.bat
@@ -1,4 +1,6 @@
 @echo off
+setlocal
+
 if "%~nx0" == "td-agent-gem.bat" (
   set "FLUENT_PACKAGE_TOPDIR=%~dp0..\"
 ) else (
@@ -10,3 +12,5 @@ for /f "usebackq" %%i in (`^""%FLUENT_PACKAGE_BINDIR%\ruby.exe" -rrbconfig -e "p
 set "GEM_HOME=%FLUENT_PACKAGE_TOPDIR%lib\ruby\gems\%RUBY_VERSION%"
 set "GEM_PATH=%FLUENT_PACKAGE_TOPDIR%lib\ruby\gems\%RUBY_VERSION%"
 "%FLUENT_PACKAGE_BINDIR%\fluent-gem" %*
+
+endlocal
