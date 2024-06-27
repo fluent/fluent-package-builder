6002cf43def30110864ca7ead596f542b56dd2dc
Daijiro Fukuda <fukuda@clear-code.com>
2024-06-27 10:38:55 +0900

    msi: refactor execution bat files
    * Add entire local scope to prevent changing global env vars
    * Remove unnecessary enabledelayedexpansion
    * Remove unnecessary `/`
    
    Signed-off-by: Daijiro Fukuda <fukuda@clear-code.com>

--- fluent-package/msi/assets/fluentd.bat
+++ fluent-package/msi/assets/fluentd.bat
@@ -1,4 +1,6 @@
 @echo off
+setlocal
+
 if "%~nx0" == "td-agent.bat" (
   set "FLUENT_PACKAGE_TOPDIR=%~dp0..\"
 ) else (
@@ -6,21 +8,25 @@ if "%~nx0" == "td-agent.bat" (
 )
 
 @rem Convert path separator from backslash to forwardslash
-setlocal enabledelayedexpansion
-set FLUENT_PACKAGE_TOPDIR=!FLUENT_PACKAGE_TOPDIR:\=/!
+set FLUENT_PACKAGE_TOPDIR=%FLUENT_PACKAGE_TOPDIR:\=/%
 
 set PATH=%FLUENT_PACKAGE_TOPDIR%bin;%PATH%
 set PATH=%FLUENT_PACKAGE_TOPDIR%;%PATH%
-set "FLUENT_CONF=%FLUENT_PACKAGE_TOPDIR%/etc/fluent/fluentd.conf"
-set "FLUENT_PLUGIN=%FLUENT_PACKAGE_TOPDIR%/etc/fluent/plugin"
-set "FLUENT_PACKAGE_VERSION=%FLUENT_PACKAGE_TOPDIR%/bin/fluent-package-version.rb"
+set "FLUENT_CONF=%FLUENT_PACKAGE_TOPDIR%etc/fluent/fluentd.conf"
+set "FLUENT_PLUGIN=%FLUENT_PACKAGE_TOPDIR%etc/fluent/plugin"
+
+setlocal
+set "FLUENT_PACKAGE_VERSION=%FLUENT_PACKAGE_TOPDIR%bin/fluent-package-version.rb"
 for %%p in (%*) do (
     if "%%p"=="--version" (
         ruby "%FLUENT_PACKAGE_VERSION%"
         goto last
     )
 )
-"%FLUENT_PACKAGE_TOPDIR%/bin/fluentd" %*
+endlocal
+
+"%FLUENT_PACKAGE_TOPDIR%bin/fluentd" %*
+
 endlocal
 
 :last
