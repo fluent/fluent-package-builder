7be83e0d889cddac7c436645040a379e5043b056
Kentaro Hayashi <hayashi@clear-code.com>
2024-07-26 15:14:21 +0900

    deb rpm: extract confluent script
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/yum/confluent-test.sh
+++ fluent-package/yum/confluent-test.sh
@@ -58,41 +58,9 @@ enabled=1
 EOF
 ${DNF} update -y && ${DNF} install -y confluent-community-2.13 nmap-ncat
 
-export KAFKA_OPTS=-Dzookeeper.4lw.commands.whitelist=ruok
-/usr/bin/zookeeper-server-start /etc/kafka/zookeeper.properties  &
-N_POLLING=30
-n=1
-while true ; do
-    sleep 1
-    status=$(echo ruok | nc localhost 2181)
-    if [ "$status" = "imok" ]; then
-	break
-    fi
-    n=$((n + 1))
-    if [ $n -ge $N_POLLING ]; then
-	echo "failed to get response from zookeeper-server"
-	exit 1
-    fi
-done
-/usr/bin/kafka-server-start /etc/kafka/server.properties &
-n=1
-while true ; do
-    sleep 1
-    status=$(/usr/bin/zookeeper-shell localhost:2181 ls /brokers/ids | sed -n 6p)
-    if [ "$status" = "[0]" ]; then
-	break
-    fi
-    n=$((n + 1))
-    if [ $n -ge $N_POLLING ]; then
-	echo "failed to get response from kafka-server"
-	exit 1
-    fi
-done
-/usr/bin/kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic test
-
-if [ "$CI" = "true" ]; then
-   echo "::endgroup::"
-fi
+CONFLUENT_SCRIPT=$(dirname $(realpath $0))/../run-confluent.sh
+echo ${CONFLUENT_SCRIPT}
+bash ${CONFLUENT_SCRIPT}
 
 if [ "$CI" = "true" ]; then
    echo "::endgroup::"
