79b4b3799ebbba388b117b32e241f8829868d097
Kentaro Hayashi <hayashi@clear-code.com>
2024-09-19 14:44:53 +0900

    deb rpm: add helper script to generate index.html for hosting site
    Currently, we use https://packages.treasuredata.com hosted on AWS
    which is powered by Treasure Data Inc.
    
    Recently, we coordinated with CNCF which provides access to Cloudflare
    R2 resource. As no S3Browser functionality, so need to replace it with
    alternative index.html.
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

---
+++ fluent-package/make-index-html.rb
@@ -0,0 +1,49 @@
+require 'erb'
+require 'find'
+require 'pathname'
+require 'optparse'
+
+options = {
+  :site => "https://fluentd.cdn.cncf.io",
+  :verbose => false
+}
+
+opt = OptionParser.new
+opt.on("-s", "--site [URL]") { |v| options[:site] = v }
+opt.on("-v", "--verbose") { |v|
+  options[:verbose] = v
+}
+top_dir = opt.parse!(ARGV).first
+
+unless File.exist?(top_dir)
+  puts "#{top_dir} not found"
+  exit 1
+end
+
+template_path = File.expand_path("#{File.basename(__FILE__, ".rb")}.erb")
+puts "Template path: #{template_path}"
+%w(5 lts test).each do |channel|
+  search_path = Pathname.new("#{top_dir}/#{channel}")
+  Find.find(search_path).each do |path|
+    Find.prune if path.end_with?(".deb")
+    Find.prune if path.end_with?(".msi")
+    Find.prune if path.end_with?(".gz")
+    Find.prune if path.end_with?(".bz2")
+    Find.prune if path.end_with?(".rpm")
+    Find.prune if path.end_with?("repodata")
+    Find.prune if path.end_with?("dists")
+    if %w(windows x86_64 aarch64 fluent-package).any? { |dir| path.end_with?(dir) }
+      puts "Updating: #{path} ..." if options[:verbose]
+      index_path = File.expand_path(File.join(path, "index.html"))
+      Dir.chdir(path) do
+        files = Dir.glob(["*.deb", "*.msi", "*.rpm"])
+        relative_path = path.sub(top_dir, "")
+        erb = ERB.new(File.read(template_path)).result(binding)
+        File.open(index_path, "w+") do |file|
+          file.puts(erb)
+          puts "Generated: #{index_path}" if options[:verbose]
+        end
+      end
+    end
+  end
+end
