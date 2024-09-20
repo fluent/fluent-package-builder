62ccb5df559a3f5dfef6b14237cca3c48b730c73
Kentaro Hayashi <hayashi@clear-code.com>
2024-09-20 11:06:05 +0900

    Simplify logic with Dir.glob
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/make-index-html.rb
+++ fluent-package/make-index-html.rb
@@ -6,7 +6,7 @@ require 'optparse'
 options = {
   site: "https://fluentd.cdn.cncf.io",
   verbose: false,
-  channel: %w(5 lts test)
+  channel: "5,lts,test"
 }
 
 opt = OptionParser.new
@@ -22,24 +22,16 @@ end
 
 template_path = File.expand_path("#{File.basename(__FILE__, ".rb")}.erb")
 puts "Template path: #{template_path}"
-options[:channel].each do |channel|
-  search_path = Pathname.new("#{top_dir}/#{channel}")
-  Find.find(search_path).each do |path|
-    Find.prune unless FileTest.directory?(path)
-    Find.prune if path.end_with?("repodata")
-    Find.prune if path.end_with?("dists")
-    if %w(windows x86_64 aarch64 fluent-package).any? { |dir| path.end_with?(dir) }
-      puts "Updating: #{path} ..." if options[:verbose]
-      index_path = File.expand_path(File.join(path, "index.html"))
-      Dir.chdir(path) do
-        files = Dir.glob(["*.deb", "*.msi", "*.rpm"])
-        relative_path = Pathname.new(path).relative_path_from(top_dir).to_s
-        erb = ERB.new(File.read(template_path)).result(binding)
-        File.open(index_path, "w+") do |file|
-          file.puts(erb)
-          puts "Generated: #{index_path}" if options[:verbose]
-        end
-      end
+Dir.glob("#{top_dir}/{#{options[:channel]}}/**/{windows,x86_64,aarch64,fluent-package}/") do |path|
+  puts "Updating: #{path} ..." if options[:verbose]
+  index_path = File.expand_path(File.join(path, "index.html"))
+  Dir.chdir(path) do
+    files = Dir.glob(["*.deb", "*.msi", "*.rpm"])
+    relative_path = Pathname.new(path).relative_path_from(top_dir).to_s
+    erb = ERB.new(File.read(template_path)).result(binding)
+    File.open(index_path, "w+") do |file|
+      file.puts(erb)
+      puts "Generated: #{index_path}" if options[:verbose]
     end
   end
 end
