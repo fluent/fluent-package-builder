42308407e1333adf45f0ec5b470d286c20b0a2ed
Kentaro Hayashi <hayashi@clear-code.com>
2024-11-07 15:54:52 +0900

    ci deb: check whether needrestart was suppressed
    Even though needrestart package was installed, service will not be
    restarted out of the maintainer script (hook).
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/apt/systemd-test/update-to-next-major-version.sh
+++ fluent-package/apt/systemd-test/update-to-next-major-version.sh
@@ -14,9 +14,23 @@ sudo /opt/fluent/bin/fluent-gem install --no-document gqtp
 
 # Install next major version
 sudo apt install -V -y \
-    /host/v6-test/${distribution}/pool/${code_name}/${channel}/*/*/fluent-package_*_${architecture}.deb
+    /host/v6-test/${distribution}/pool/${code_name}/${channel}/*/*/fluent-package_*_${architecture}.deb 2>&1 | tee upgrade.log
+
+# Test: needrestart was suppressed
+if dpkg-query --show --showformat='${Version}' needrestart ; then
+  case $code_name in
+    focal)
+      # dpkg-query succeeds even though needrestart is not installed.
+      (! grep "No services need to be restarted." upgrade.log)
+      ;;
+    *)
+      grep "No services need to be restarted." upgrade.log
+      ;;
+  esac
+fi
 
 # Test: Check whether plugin/gem were installed during upgrading
 /opt/fluent/bin/fluent-gem list | grep fluent-plugin-concat
 # Non fluent-plugin- prefix gem should not be installed automatically
 (! /opt/fluent/bin/fluent-gem list | grep gqtp)
+
