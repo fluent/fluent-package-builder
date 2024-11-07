6f463541175c858ad152ab349284fb7ea0eda07b
Shizuo Fujita <fujita@clear-code.com>
2024-11-07 13:37:16 +0900

    ci: send data until main process is replaced
    Signed-off-by: Shizuo Fujita <fujita@clear-code.com>

--- fluent-package/apt/systemd-test/update-to-next-version-without-data-lost.sh
+++ fluent-package/apt/systemd-test/update-to-next-version-without-data-lost.sh
@@ -26,7 +26,7 @@ sleep 1
 
 # Send logs in background for 4 seconds
 /opt/fluent/bin/ruby $(dirname $0)/../../test-tools/logdata-sender.rb \
-    --udp-data-count 50 --tcp-data-count 60 --syslog-data-count 70 --syslog-identifer "test-syslog" --duration 4 &
+    --udp-data-count 50 --tcp-data-count 60 --syslog-data-count 70 --syslog-identifer "test-syslog" --duration 16 &
 
 sleep 1
 
@@ -35,10 +35,12 @@ sudo apt install -V -y \
     /host/v6-test/${distribution}/pool/${code_name}/${channel}/*/*/fluent-package_*_${architecture}.deb
 test $main_pid -eq $(systemctl show --value --property=MainPID fluentd)
 
-sleep 3
+# Main process should be replaced by USR2 signal
+sleep 20
+test $main_pid -ne $(eval $(systemctl show fluentd --property=MainPID) && echo $MainPID)
 
 # Stop fluentd to flush the logs and check
 systemctl stop fluentd
-test $(wc -l /var/log/fluent/test_udp*.log | cut -d' ' -f 1) = "50"
-test $(wc -l /var/log/fluent/test_tcp*.log | cut -d' ' -f 1) = "60"
+test $(wc -l /var/log/fluent/test_udp*.log | tail -n 1 | awk '{print $1}') = "50"
+test $(wc -l /var/log/fluent/test_tcp*.log | tail -n 1 | awk '{print $1}') = "60"
 test $(grep "test-syslog" /var/log/fluent/test_syslog*.log | wc -l) = "70"
