8127df7c097ac0b2487a5e12bda65f960e2ec990
Daijiro Fukuda <fukuda@clear-code.com>
2024-11-19 17:38:15 +0900

    replace Fluentd for 1.18 test
    Signed-off-by: Daijiro Fukuda <fukuda@clear-code.com>

--- fluent-package/config.rb
+++ fluent-package/config.rb
@@ -7,7 +7,7 @@ COMPAT_SERVICE_NAME = "td-agent"
 PACKAGE_DIR = "fluent"
 COMPAT_PACKAGE_DIR = COMPAT_SERVICE_NAME
 
-FLUENTD_REVISION = 'f8755d09b747d054302c0e12a3b0962a11f1045e' # test no-downtime (TODO fix later)
+FLUENTD_REVISION = '759bb582a1d4a1b1d163197870669f7bee0aa148' # test no-downtime (TODO fix later)
 FLUENTD_LOCAL_GEM_REPO = "file://" + File.expand_path(File.join(__dir__, "local_gem_repo"))
 
 # https://github.com/jemalloc/jemalloc/releases
