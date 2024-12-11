038b7704db75ce64401e63dcffd233b258b37ea2
Daijiro Fukuda <fukuda@clear-code.com>
2024-12-11 16:18:10 +0900

    rpm: improve process timing for safety (#764)
    Points
    
    * FROM-package just leaves tmp files if it supports the features.
    * TO-package trigger the features if there are those tmp files.
    * Thus, can ensure that both FROM and TO support the feature.
    * Make installing plugin and restarting the same condition.
    * Disable `%systemd_postun_with_restart` completely to align
    specifications with DEB.
    
    Before
    
    1. to-pre(2): Collect plugin-list.
    2. Install TO-package
    3. to-post(2): Install plugin.
    4. from-preun(1): Do nothing.
    5. Uninstall FROM-package
    6. from-postun(1): Restart if need.
    
    After
    
    1. to-pre(2): Collect plugin-list.
    2. Install TO-package
    3. to-post(2): Do nothing.
    4. from-preun(1):
       * Check auto or not.
       * Leave plugin-install flag and pid if need.
    5. Uninstall FROM-package
    6. from-postun(1): Disable `%systemd_postun_with_restart`.
    7. to-posttrans: Install plugin and restart if need.
    
    Signed-off-by: Daijiro Fukuda <fukuda@clear-code.com>

--- fluent-package/yum/systemd-test/update-to-next-major-version.sh
+++ fluent-package/yum/systemd-test/update-to-next-major-version.sh
@@ -5,10 +5,15 @@ set -exu
 . $(dirname $0)/commonvar.sh
 
 service_restart=$1
+status_before_update=$2 # active / inactive
 
 # Install the current
 package="/host/${distribution}/${DISTRIBUTION_VERSION}/x86_64/Packages/fluent-package-*.rpm"
 sudo $DNF install -y $package
+
+if [ "$status_before_update" = active ]; then
+    sudo systemctl start fluentd
+fi
 
 # Set FLUENT_PACKAGE_SERVICE_RESTART
 sed -i "s/=auto/=$service_restart/" /etc/sysconfig/fluentd
@@ -22,7 +27,7 @@ package="/host/v6-test/${distribution}/${DISTRIBUTION_VERSION}/x86_64/Packages/f
 sudo $DNF install -y $package
 
 # Test: Check whether plugin/gem were installed during upgrading
-if [ "$service_restart" = auto ]; then
+if [ "$service_restart" = auto ] && [ "$status_before_update" = active ]; then
     # plugin gem should be installed automatically
     /opt/fluent/bin/fluent-gem list | grep fluent-plugin-concat
     # Non fluent-plugin- prefix gem should not be installed automatically
