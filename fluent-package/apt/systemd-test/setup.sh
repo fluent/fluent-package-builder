82e56c5c17815a861965c112ebb489d6c1a03cff
Kentaro Hayashi <hayashi@clear-code.com>
2025-02-14 18:13:28 +0900

    ci: use apt-eatmydata equivalent configuration
    apt-eatmydata reduces disk IO, let's try it
    for CI.
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/apt/systemd-test/setup.sh
+++ fluent-package/apt/systemd-test/setup.sh
@@ -3,4 +3,10 @@
 set -exu
 
 sudo apt update
+# Setup apt-eatmydata equivalent configuration
+sudo apt install -y eatmydata
+sudo ln -s /usr/bin/eatmydata /usr/local/bin/dpkg
+sudo tee /etc/apt/apt.conf.d/25-eatmydata-action << 'EOF'
+Dir::Bin::dpkg "/usr/local/bin/dpkg";
+EOF
 sudo apt install -V -y lsb-release curl
