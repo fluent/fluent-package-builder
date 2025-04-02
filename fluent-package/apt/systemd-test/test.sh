b17581031c356d7afcf32d7090ce6a3649700b83
Kentaro Hayashi <hayashi@clear-code.com>
2025-04-02 17:27:38 +0900

    ci: use Incus instead of LXD (#803)
    It seems that AmazonLinux:2 container image is not available anymore
    from https://images.lxd.canonical.com/.
    AmazonLinux:2 is still available from
    https://images.linuxcontainers.org,
    but it can't be added without interactive operation for LXD.
    
    It seems that it is easy to use Incus as a workaround.
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/apt/systemd-test/test.sh
+++ fluent-package/apt/systemd-test/test.sh
@@ -15,21 +15,21 @@ dir="/host/fluent-package/apt/systemd-test"
 set -eux
 
 echo "::group::Run test: launch $image"
-lxc launch $image target --debug
+sudo incus launch $image target --debug
 sleep 5
 echo "::endgroup::"
 echo "::group::Run test: configure $image"
-lxc config device add target host disk source=$PWD path=/host
-lxc list
+sudo incus config device add target host disk source=$PWD path=/host
+sudo incus list
 echo "::endgroup::"
 echo "::group::Run test: setup $image"
-lxc exec target -- $dir/setup.sh
+sudo incus exec target -- $dir/setup.sh
 echo "::endgroup::"
 echo "::group::Run test: $test_file $other_args on $image"
-lxc exec target -- $dir/$test_file $other_args
+sudo incus exec target -- $dir/$test_file $other_args
 echo "::endgroup::"
 echo "::group::Run test: cleanup $image"
-lxc stop target
-lxc delete target
+sudo incus stop target
+sudo incus delete target
 echo "::endgroup::"
 echo -e "\nAll Success!\n"
