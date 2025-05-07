9846e2fd67ad24abea61b492c7db95b1b9412859
Kentaro Hayashi <hayashi@clear-code.com>
2025-05-07 15:02:19 +0900

    Backport: ci: use Incus instead of LXD (#803) (#818)
    Backport #803 
    
    It seems that AmazonLinux:2 container image is not available anymore
    from https://images.lxd.canonical.com/.
    AmazonLinux:2 is still available from
    https://images.linuxcontainers.org,
    but it can't be added without interactive operation for LXD.
    
    It seems that it is easy to use Incus as a workaround.
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/yum/systemd-test/test.sh
+++ fluent-package/yum/systemd-test/test.sh
@@ -14,18 +14,18 @@ dir="/host/fluent-package/yum/systemd-test"
 set -eux
 
 echo "::group::Run test: launch $image"
-lxc launch $image target --debug
+sudo incus launch $image target --debug
 sleep 5
 echo "::endgroup::"
 echo "::group::Run test: configure $image"
-lxc config device add target host disk source=$PWD path=/host
-lxc list
+sudo incus config device add target host disk source=$PWD path=/host
+sudo incus list
 echo "::endgroup::"
-echo "::group::Run test: $test_file $yum_repo_type on $image"
-lxc exec target -- $dir/$test_file $yum_repo_type
+echo "::group::Run test: $test_file $other_args on $image"
+sudo incus exec target -- $dir/$test_file $other_args
 echo "::endgroup::"
 echo "::group::Run test: cleanup $image"
-lxc stop target
-lxc delete target
+sudo incus stop target
+sudo incus delete target
 echo "::endgroup::"
 echo -e "\nAll Success!\n"
