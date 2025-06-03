ae2f8851f9cb1505ad077c092473cca7c31a98fc
Kentaro Hayashi <hayashi@clear-code.com>
2025-06-03 10:19:57 +0900

    msi: recommend Fluentd enterprise service (#847)
    In the previous versions, there were no navigation to access Fluentd
    enterprise services.
    
    It might be useful to provide such navigation link for Fluentd users.
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

---
+++ fluent-package/msi/assets/fluent-package-post-toast.bat
@@ -0,0 +1,7 @@
+@echo off
+title Fluent-package post toast script
+if exist "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" (
+  if exist "%~dp0..\bin\fluent-package-post-toast.ps1" (
+    "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" "%~dp0..\bin\fluent-package-post-toast.ps1"
+  )
+)
