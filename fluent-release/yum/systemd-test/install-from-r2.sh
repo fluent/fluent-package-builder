d47abe41a968202dcaf369e6389873cdf4524555
Kentaro Hayashi <hayashi@clear-code.com>
2025-07-09 10:01:46 +0900

    Add fluent-release package for yum/dnf update (#845)
    rpm: add fluent-release package for yum/dnf update
    
    In the previous versions, .repo file was assumed to
    be created via fluent-package installation script.
    
    For migration from packages.treasuredata.com to fluentd.cdn.cncf.io
    in the future, it might be better to setup proper fluent-release
    package for it.
    
    ---------
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>
    Co-authored-by: Daijiro Fukuda <fukuda@clear-code.com>

---
+++ fluent-release/yum/systemd-test/install-from-r2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -exu
+
+. $(dirname $0)/../../../fluent-package/yum/systemd-test/commonvar.sh
+
+sudo $DNF install -y \
+     /host/${distribution}/${DISTRIBUTION_VERSION}/x86_64/Packages/fluent-release-*.noarch.rpm
+
+test -f /etc/pki/rpm-gpg/RPM-GPG-KEY-td-agent
+test -f /etc/pki/rpm-gpg/RPM-GPG-KEY-fluent-package
+
+test -f /etc/yum.repos.d/fluent-package.repo
+grep fluentd.cdn.cncf.io /etc/yum.repos.d/fluent-package.repo
+test $($DNF repolist --enabled | grep 'Fluentd Project' | wc -l) -eq 1
+test $($DNF repolist --enabled | grep 'Fluentd Project' | cut -d' ' -f1) = fluent-package-v6
+
+# TODO: v6 package was released, remove it and test with v6
+sudo $DNF config-manager --disable fluent-package-v6
+sudo $DNF config-manager --enable fluent-package-v5
+
+sudo $DNF update -y
+sudo $DNF install -y fluent-package
