0d4617be530d2eea97a302a7d4caf7fc7b12bb99
Shizuo Fujita <fujita@clear-code.com>
2025-07-23 17:32:43 +0900

    config: update fluentd revision
    Signed-off-by: Shizuo Fujita <fujita@clear-code.com>

--- fluent-package/config.rb
+++ fluent-package/config.rb
@@ -7,7 +7,7 @@ COMPAT_SERVICE_NAME = "td-agent"
 PACKAGE_DIR = "fluent"
 COMPAT_PACKAGE_DIR = COMPAT_SERVICE_NAME
 
-FLUENTD_REVISION = 'bee06706104aad4ee243f2e959c6d18005762b35' # HEAD #4993
+FLUENTD_REVISION = 'cefbc62e21570c4761a78297d481adff51e36692' # HEAD #5031
 FLUENTD_LOCAL_GEM_REPO = "file://" + File.expand_path(File.join(__dir__, "local_gem_repo"))
 
 # https://github.com/jemalloc/jemalloc/releases
