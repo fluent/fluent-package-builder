8a93aa18212e74cd01b787c395bcc22e4e91e64d
Kentaro Hayashi <hayashi@clear-code.com>
2025-07-25 18:37:01 +0900

    Follow to release v6 packages (#867)
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/manage-fluent-repositories.sh
+++ fluent-package/manage-fluent-repositories.sh
@@ -74,7 +74,7 @@ case $COMMAND in
 	$command
 	;;
     dry-upload|upload)
-	TARGETS="amazon redhat windows macosx debian/bookworm debian/trixie ubuntu/jammy ubuntu/focal ubuntu/noble"
+	TARGETS="amazon redhat windows debian/bookworm debian/trixie ubuntu/jammy ubuntu/noble"
 	DRYRUN_OPTION="--dryrun"
 	if [ $COMMAND = "upload" ]; then
 	   DRYRUN_OPTION=""
@@ -82,16 +82,16 @@ case $COMMAND in
 	for target in $TARGETS; do
 	    case $FLUENT_RELEASE_DIR in
 		*test/experimental/lts)
-		    command="aws s3 sync $DRYRUN_OPTION --delete $FLUENT_RELEASE_DIR/5/$target s3://packages.treasuredata.com/test/experimental/lts/5/$target --profile $FLUENT_RELEASE_PROFILE"
+		    command="aws s3 sync $DRYRUN_OPTION --delete $FLUENT_RELEASE_DIR/6/$target s3://packages.treasuredata.com/test/experimental/lts/6/$target --profile $FLUENT_RELEASE_PROFILE"
 		    ;;
 		*test/experimental)
-		    command="aws s3 sync $DRYRUN_OPTION --delete $FLUENT_RELEASE_DIR/5/$target s3://packages.treasuredata.com/test/experimental/5/$target --profile $FLUENT_RELEASE_PROFILE"
+		    command="aws s3 sync $DRYRUN_OPTION --delete $FLUENT_RELEASE_DIR/6/$target s3://packages.treasuredata.com/test/experimental/6/$target --profile $FLUENT_RELEASE_PROFILE"
 		    ;;
 		*lts)
-		    command="aws s3 sync $DRYRUN_OPTION --delete $FLUENT_RELEASE_DIR/5/$target s3://packages.treasuredata.com/lts/5/$target --profile $FLUENT_RELEASE_PROFILE"
+		    command="aws s3 sync $DRYRUN_OPTION --delete $FLUENT_RELEASE_DIR/6/$target s3://packages.treasuredata.com/lts/6/$target --profile $FLUENT_RELEASE_PROFILE"
 		    ;;
 		*)
-		    command="aws s3 sync $DRYRUN_OPTION --delete $FLUENT_RELEASE_DIR/5/$target s3://packages.treasuredata.com/5/$target --profile $FLUENT_RELEASE_PROFILE"
+		    command="aws s3 sync $DRYRUN_OPTION --delete $FLUENT_RELEASE_DIR/6/$target s3://packages.treasuredata.com/6/$target --profile $FLUENT_RELEASE_PROFILE"
 		    ;;
 	    esac
 	    echo $command
@@ -139,27 +139,27 @@ EOF
 	sleep 10
 	export GPG_TTY=$(tty)
 	for d in bookworm trixie jammy noble; do
-	    aptly -config="$aptly_conf" repo create -distribution=$d -component=contrib fluent-package5-$d
+	    aptly -config="$aptly_conf" repo create -distribution=$d -component=contrib fluent-package6-$d
 	    case $d in
 		bookworm|trixie)
-		    aptly -config="$aptly_conf" repo add fluent-package5-$d $FLUENT_RELEASE_DIR/5/debian/$d/
-		    aptly -config="$aptly_conf" snapshot create fluent-package5-$d-${FLUENT_PACKAGE_VERSION}-1 from repo fluent-package5-$d
+		    aptly -config="$aptly_conf" repo add fluent-package6-$d $FLUENT_RELEASE_DIR/6/debian/$d/
+		    aptly -config="$aptly_conf" snapshot create fluent-package6-$d-${FLUENT_PACKAGE_VERSION}-1 from repo fluent-package6-$d
 		    # publish snapshot with prefix, InRelease looks like (e.g. bullseye):
 		    #   Origin: bullseye bullseye
 		    #   Label: bullseye bullseye
-		    aptly -config="$aptly_conf" publish snapshot -component=contrib -gpg-key=$SIGNING_KEY fluent-package5-$d-${FLUENT_PACKAGE_VERSION}-1 $d
+		    aptly -config="$aptly_conf" publish snapshot -component=contrib -gpg-key=$SIGNING_KEY fluent-package6-$d-${FLUENT_PACKAGE_VERSION}-1 $d
 		    # Place generated files, package files themselves are already in there
-		    tar cf - --exclude="td-agent_*.deb" --exclude="fluent-package_*.deb" -C "$aptly_rootdir/public" $d | tar xvf - -C $FLUENT_RELEASE_DIR/5/debian/
+		    tar cf - --exclude="td-agent_*.deb" --exclude="fluent-package_*.deb" -C "$aptly_rootdir/public" $d | tar xvf - -C $FLUENT_RELEASE_DIR/6/debian/
 		    ;;
 		jammy|noble)
-		    aptly -config="$aptly_conf" repo add fluent-package5-$d $FLUENT_RELEASE_DIR/5/ubuntu/$d/
-		    aptly -config="$aptly_conf" snapshot create fluent-package5-$d-${FLUENT_PACKAGE_VERSION}-1 from repo fluent-package5-$d
+		    aptly -config="$aptly_conf" repo add fluent-package6-$d $FLUENT_RELEASE_DIR/6/ubuntu/$d/
+		    aptly -config="$aptly_conf" snapshot create fluent-package6-$d-${FLUENT_PACKAGE_VERSION}-1 from repo fluent-package6-$d
 		    # publish snapshot with prefix, InRelease looks like (e.g. focal):
 		    #   Origin: focal focal
 		    #   Label: focal focal
-		    aptly -config="$aptly_conf" publish snapshot -component=contrib -gpg-key=$SIGNING_KEY fluent-package5-$d-${FLUENT_PACKAGE_VERSION}-1 $d
+		    aptly -config="$aptly_conf" publish snapshot -component=contrib -gpg-key=$SIGNING_KEY fluent-package6-$d-${FLUENT_PACKAGE_VERSION}-1 $d
 		    # Place generated files, package files themselves are already in there
-		    tar cf - --exclude="td-agent_*.deb" --exclude="fluent-package_*.deb" -C "$aptly_rootdir/public" $d | tar xvf - -C $FLUENT_RELEASE_DIR/5/ubuntu/
+		    tar cf - --exclude="td-agent_*.deb" --exclude="fluent-package_*.deb" -C "$aptly_rootdir/public" $d | tar xvf - -C $FLUENT_RELEASE_DIR/6/ubuntu/
 		    ;;
 	    esac
 	done
@@ -167,17 +167,17 @@ EOF
 	;;
     rpm)
 	# resign rpm packages
-	find $FLUENT_RELEASE_DIR/5 -name "*$FLUENT_PACKAGE_VERSION*.rpm"
+	find $FLUENT_RELEASE_DIR/6 -name "*$FLUENT_PACKAGE_VERSION*.rpm"
 	echo "Ready to type signing passphrase? (process starts in 10 seconds, Ctrl+C to abort)"
 	sleep 10
 	export GPG_TTY=$(tty)
-	find $FLUENT_RELEASE_DIR/5 -name "*$FLUENT_PACKAGE_VERSION*.rpm" | xargs rpm --resign --define "_gpg_name $SIGNING_KEY"
+	find $FLUENT_RELEASE_DIR/6 -name "*$FLUENT_PACKAGE_VERSION*.rpm" | xargs rpm --resign --define "_gpg_name $SIGNING_KEY"
 	# check whether packages are signed correctly.
-	find $FLUENT_RELEASE_DIR/5 -name "*$FLUENT_PACKAGE_VERSION*.rpm" | xargs rpm -K || \
+	find $FLUENT_RELEASE_DIR/6 -name "*$FLUENT_PACKAGE_VERSION*.rpm" | xargs rpm -K || \
 	    (echo "Import public key to verify: rpm --import https://s3.amazonaws.com/packages.treasuredata.com/GPG-KEY-fluent-package" && exit 1)
 
 	# update & sign rpm repository
-	repodirs=`find "${FLUENT_RELEASE_DIR}" -regex "^${FLUENT_RELEASE_DIR}/5/\(redhat\|amazon\)/\([2789]\|2023\)/\(x86_64\|aarch64\)$"`
+	repodirs=`find "${FLUENT_RELEASE_DIR}" -regex "^${FLUENT_RELEASE_DIR}/6/\(redhat\|amazon\)/\([2789]\|10\|2023\)/\(x86_64\|aarch64\)$"`
 	for repodir in $repodirs; do
 	    createrepo_c -v --compatibility "${repodir}"
 
