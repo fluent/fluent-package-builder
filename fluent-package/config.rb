84afb34e837e5356f09619f79a0595ed242006da
Shizuo Fujita <fujita@clear-code.com>
2025-11-05 18:05:32 +0900

    Add fluent-package 6.0.1
    Signed-off-by: Shizuo Fujita <fujita@clear-code.com>

--- fluent-package/config.rb
+++ fluent-package/config.rb
@@ -1,5 +1,5 @@
 PACKAGE_NAME = "fluent-package"
-PACKAGE_VERSION = "6.0.0"
+PACKAGE_VERSION = "6.0.1"
 
 # Keep internal path (/opt/td-agent) for package name migration
 SERVICE_NAME = "fluentd"
@@ -7,7 +7,7 @@ COMPAT_SERVICE_NAME = "td-agent"
 PACKAGE_DIR = "fluent"
 COMPAT_PACKAGE_DIR = COMPAT_SERVICE_NAME
 
-FLUENTD_REVISION = 'ed15edc4b7c86c1fcea3fbf326b7c42325e6fc71' # v1.19.0
+FLUENTD_REVISION = 'efdc4dca81c23480c9b55e13e55de6aa925b1cf5' # v1.19.1
 FLUENTD_LOCAL_GEM_REPO = "file://" + File.expand_path(File.join(__dir__, "local_gem_repo"))
 
 # https://github.com/jemalloc/jemalloc/releases
@@ -23,8 +23,8 @@ OPENSSL_FOR_MACOS_SHA256SUM = "6c13d2bf38fdf31eac3ce2a347073673f5d63263398f1f69d
 BUNDLER_VERSION= "2.3.27"
 
 # https://www.ruby-lang.org/en/downloads/ (tar.gz)
-BUNDLED_RUBY_VERSION = "3.4.5"
-BUNDLED_RUBY_SOURCE_SHA256SUM = "1d88d8a27b442fdde4aa06dc99e86b0bbf0b288963d8433112dd5fac798fd5ee"
+BUNDLED_RUBY_VERSION = "3.4.7"
+BUNDLED_RUBY_SOURCE_SHA256SUM = "23815a6d095696f7919090fdc3e2f9459b2c83d57224b2e446ce1f5f7333ef36"
 
 BUNDLED_RUBY_PATCHES = [
   # An example entry:
@@ -32,8 +32,8 @@ BUNDLED_RUBY_PATCHES = [
 ]
 
 # https://rubyinstaller.org/downloads/ (7-ZIP ARCHIVES)
-BUNDLED_RUBY_INSTALLER_X64_VERSION = "3.4.5-1"
-BUNDLED_RUBY_INSTALLER_X64_SHA256SUM = "61634b204f808deec191a6819e4c25c7b3005d84537b24c4fbe7c604aeece89f"
+BUNDLED_RUBY_INSTALLER_X64_VERSION = "3.4.7-1"
+BUNDLED_RUBY_INSTALLER_X64_SHA256SUM = "955678a8373c23e53032ac10f730a06120c7d3b956074020e57ca8b99b8930d4"
 
 # Files under rubyinstaller/ are patches for RubyInstaller's binary package.
 # Other patches for Ruby's source tree which can be shared with BUNDLED_RUBY_PATCHES.
