e75be931965495aa94da0f7f86730e443a749086
Shizuo Fujita <fujita@clear-code.com>
2025-11-06 16:11:48 +0900

    Backport(v6): msi: fix bug where install fails for path with spaces (#927) (#931)
    Backport https://github.com/fluent/fluent-package-builder/pull/927
    
    * Fixes #926
    
    Signed-off-by: Daijiro Fukuda <fukuda@clear-code.com>
    Signed-off-by: Shizuo Fujita <fujita@clear-code.com>
    Co-authored-by: Daijiro Fukuda <fukuda@clear-code.com>

--- fluent-package/msi/assets/fluent-package-post-toast.bat
+++ fluent-package/msi/assets/fluent-package-post-toast.bat
@@ -2,6 +2,6 @@
 title Fluent-package post toast script
 if exist "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" (
   if exist "%~dp0..\bin\fluent-package-post-toast.ps1" (
-    "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" "%~dp0..\bin\fluent-package-post-toast.ps1"
+    "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -File "%~dp0..\bin\fluent-package-post-toast.ps1"
   )
 )
