d7dbd1e1e6cac03a2c8b2a2f08671cad1636831d
Kentaro Hayashi <hayashi@clear-code.com>
2026-02-13 13:55:54 +0900

    Update bundled versions
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/config.rb
+++ fluent-package/config.rb
@@ -7,7 +7,7 @@ COMPAT_SERVICE_NAME = "td-agent"
 PACKAGE_DIR = "fluent"
 COMPAT_PACKAGE_DIR = COMPAT_SERVICE_NAME
 
-FLUENTD_REVISION = 'efdc4dca81c23480c9b55e13e55de6aa925b1cf5' # v1.19.1
+FLUENTD_REVISION = '76841666b4e4aade23f0c100a7e048995f7d52c8' # v1.19.2
 FLUENTD_LOCAL_GEM_REPO = "file://" + File.expand_path(File.join(__dir__, "local_gem_repo"))
 
 # https://github.com/jemalloc/jemalloc/releases
@@ -23,8 +23,8 @@ OPENSSL_FOR_MACOS_SHA256SUM = "6c13d2bf38fdf31eac3ce2a347073673f5d63263398f1f69d
 BUNDLER_VERSION= "2.3.27"
 
 # https://www.ruby-lang.org/en/downloads/ (tar.gz)
-BUNDLED_RUBY_VERSION = "3.4.7"
-BUNDLED_RUBY_SOURCE_SHA256SUM = "23815a6d095696f7919090fdc3e2f9459b2c83d57224b2e446ce1f5f7333ef36"
+BUNDLED_RUBY_VERSION = "3.4.8"
+BUNDLED_RUBY_SOURCE_SHA256SUM = "53c4ddad41fbb6189f1f5ee0db57a51d54bd1f87f8755b3d68604156a35b045b"
 
 BUNDLED_RUBY_PATCHES = [
   # An example entry:
@@ -32,8 +32,8 @@ BUNDLED_RUBY_PATCHES = [
 ]
 
 # https://rubyinstaller.org/downloads/ (7-ZIP ARCHIVES)
-BUNDLED_RUBY_INSTALLER_X64_VERSION = "3.4.7-1"
-BUNDLED_RUBY_INSTALLER_X64_SHA256SUM = "955678a8373c23e53032ac10f730a06120c7d3b956074020e57ca8b99b8930d4"
+BUNDLED_RUBY_INSTALLER_X64_VERSION = "3.4.8-1"
+BUNDLED_RUBY_INSTALLER_X64_SHA256SUM = "d1c3ba83ae748c08e35e0b1d9939d45dbca7925e0a8bf84a42860bf19847e0d6"
 
 # Files under rubyinstaller/ are patches for RubyInstaller's binary package.
 # Other patches for Ruby's source tree which can be shared with BUNDLED_RUBY_PATCHES.
