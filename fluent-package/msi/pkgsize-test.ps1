a1f2b5b1cfe0c4c82398747673ed57e0af837884
Kentaro Hayashi <hayashi@clear-code.com>
2026-02-16 13:57:21 +0900

    Backport(v6): msi: use CloudFlare R2 first
    backport #948, #962
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/msi/pkgsize-test.ps1
+++ fluent-package/msi/pkgsize-test.ps1
@@ -1,22 +1,27 @@
 $ErrorActionPreference = 'Stop'
 
-$base_uri = "http://packages.treasuredata.com.s3.amazonaws.com"
+$PSVersionTable
+
+$base_uri = "https://fluentd.cdn.cncf.io"
 &git fetch --unshallow
+$previous_version = (git describe --abbrev=0 --tags) -Replace "v",""
+$major_version = ([version] $previous_version).Major
 try {
-    $previous_version = (git describe --abbrev=0 --tags) -Replace "v",""
     "Previous version from git: {0}" -F $previous_version | Write-Host
-    $previous_msi_name = "td-agent-${previous_version}-x64.msi"
-    $response = Invoke-WebRequest -Uri "${base_uri}/4/windows/${previous_msi_name}" -OutFile $previous_msi_name -PassThru
+    $previous_msi_name = "fluent-package-${previous_version}-x64.msi"
+    $response = Invoke-WebRequest -UseBasicParsing -Uri "${base_uri}/lts/${major_version}/windows/${previous_msi_name}" -OutFile $previous_msi_name -PassThru
 }
 catch {
-    $heroku_uri = 'http://td-agent-package-browser.herokuapp.com'
+    $heroku_uri = 'https://td-agent-package-browser.herokuapp.com'
     Write-Host "An exception was caught: $($_.Exception.Message). Try to find previous version in ${heroku_uri} instead"
-    $msi_links = (Invoke-WebRequest -Uri "${heroku_uri}/4/windows").Links.href | Where-Object {$_ -like "*.msi"}
+    "Checking package major version: {0}" -F $major_version | Write-Host
+    "Checking {0}/{1}/windows" -F $heroku_uri, $major_version | Write-Host
+    $msi_links = (Invoke-WebRequest -UseBasicParsing -Uri "${heroku_uri}/lts/${major_version}/windows").Links.href | Where-Object {$_ -like "*.msi"}
     $msi_versions = $($msi_links | Select-String '(\d+\.\d+\.\d+)' | ForEach-Object { $_.Matches[0].Groups[1].Value }) | Sort-Object {[version] $_}
     $previous_version = $msi_versions | Select-Object -Last 1
     "Previous version from {0}: {1}" -F ${heroku_uri}, $previous_version | Write-Host
-    $previous_msi_name = "td-agent-${previous_version}-x64.msi"
-    $response = Invoke-WebRequest -Uri "${base_uri}/4/windows/${previous_msi_name}" -OutFile $previous_msi_name -PassThru
+    $previous_msi_name = "fluent-package-${previous_version}-x64.msi"
+    $response = Invoke-WebRequest -UseBasicParsing -Uri "${base_uri}/lts/${major_version}/windows/${previous_msi_name}" -OutFile $previous_msi_name -PassThru
 }
 
 $msi = (Get-Item "fluent-package\\msi\\repositories\\fluent-package-*.msi") | Sort-Object -Descending { $_.LastWriteTime } | Select-Object -First 1
