8b4b674807ed013a5ce65faa63d677bb36d29b07
Kentaro Hayashi <hayashi@clear-code.com>
2026-02-26 14:26:32 +0900

    Backport(v6) msi: change ExecutionPolicy to enable it (#982)
    In the previous versions, when RemoteSigned was Restricted, it fails
    to execute toast.ps1, thus it causes installation rollback unexpectedly.
        So, it should specify policy of powershell explicitly
    
    Even though execution was disabled via GPO, it just fails but
        always fallback to exit /b 0.
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/msi/assets/fluent-package-post-toast.bat
+++ fluent-package/msi/assets/fluent-package-post-toast.bat
@@ -2,6 +2,7 @@
 title Fluent-package post toast script
 if exist "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" (
   if exist "%~dp0..\bin\fluent-package-post-toast.ps1" (
-    "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -File "%~dp0..\bin\fluent-package-post-toast.ps1"
+    "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy Bypass -File "%~dp0..\bin\fluent-package-post-toast.ps1"
   )
 )
+exit /b 0
