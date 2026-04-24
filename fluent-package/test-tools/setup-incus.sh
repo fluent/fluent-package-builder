78b9cacf4debc4b7b8bb04707ed46203474cc807
Kentaro Hayashi <hayashi@clear-code.com>
2026-04-24 13:38:42 +0900

    apt: enable ubuntu 26.04 (#1024)
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/test-tools/setup-incus.sh
+++ fluent-package/test-tools/setup-incus.sh
@@ -9,3 +9,5 @@ sudo iptables -I DOCKER-USER -i incusbr0 -j ACCEPT
 sudo iptables -I DOCKER-USER -o incusbr0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 sudo incus admin init --auto
 
+# Enable custom incus images
+sudo incus remote add fluent https://fluentd.cdn.cncf.io/test/incus-images --accept-certificate --protocol=simplestreams
