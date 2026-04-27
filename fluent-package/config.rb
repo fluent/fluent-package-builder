61e7b40d51051f6d819e2c0c5100d990a181027e
Kentaro Hayashi <hayashi@clear-code.com>
2026-04-27 11:02:33 +0900

    Bump version to snapshot
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- fluent-package/config.rb
+++ fluent-package/config.rb
@@ -7,7 +7,7 @@ COMPAT_SERVICE_NAME = "td-agent"
 PACKAGE_DIR = "fluent"
 COMPAT_PACKAGE_DIR = COMPAT_SERVICE_NAME
 
-FLUENTD_REVISION = '76841666b4e4aade23f0c100a7e048995f7d52c8' # v1.19.2
+FLUENTD_REVISION = '1b849cb95719eefd72ac37213797d4b6d15ea9e1' # v1.19.3 (RC Apr 20)
 FLUENTD_LOCAL_GEM_REPO = "file://" + File.expand_path(File.join(__dir__, "local_gem_repo"))
 
 # https://github.com/jemalloc/jemalloc/releases
